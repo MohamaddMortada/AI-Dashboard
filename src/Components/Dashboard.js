671809b563560b4637c9ddad190c3dc147373070
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-11 23:32:14 +0200

    Fixing Bugs

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,56 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import axios from '../axios';
-import UserCard from './UserCard';
-import '../Stylings/Dashboard.css'; 
+import '../Stylings/Dashboard.css';
 import UsersBoard from './Users';
 import DetectionBoard from './DetectionBoard';
 import PredictionBoard from './PredictionBoard';
 import CalculatingBoard from './CalculationBoard';
 import ChatBotBoard from './ChatbotBoard';
 import ResultBoard from './ResultsBoard';
+import FinishBoard from './FinishBoard';
 const Dashboard = () => {
-    const [users, setUsers] = useState([]);
-    const [stats, setStats] = useState({
-      detecting: 0,
-      predicting: 0,
-      chatbot: 0,
-      calculating: 0,
-      photoFinish: 0,
-      addingResults: 0,
-    });
-  
-    useEffect(() => {
-      axios.get('http://127.0.0.1:8000/api/users')
-        .then(response => {
-          setUsers(response.data.users); 
-        })
-        .catch(error => {
-          console.error('Error fetching user data', error);
-        });
-
-        axios.get('/stats')
-      .then(response => {
-        setStats(response.data);
-      })
-      .catch(error => {
-        console.error('Error fetching stats data', error);
-      });
-
-    }, []);
-return (
-    <div className="dashboard">
-      <h1>AI Dashboard</h1>
-      <div className="stats">
-        <DetectionBoard/>
-        <PredictionBoard/>
-        <CalculatingBoard/>
-        <ChatBotBoard/>
-        <ResultBoard/>
-        </div>
-
-        <UsersBoard/>
-      
-    </div>
-)
+    return (
+        <div className="dashboard">
+            <h1>AI Dashboard</h1>
+            <div className="stats">
+                <DetectionBoard />
+                <PredictionBoard />
+                <CalculatingBoard />
+                <ChatBotBoard />
+                <ResultBoard />
+                <FinishBoard />
+            </div>
+            <UsersBoard />
+            </div>
+    )
 }
 export default Dashboard;
