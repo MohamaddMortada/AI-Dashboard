10fa25fd794c46df0b2de81cd18357e7a97bd6a6
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-11 19:04:41 +0200

    Creating Dashboard export

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import axios from '../axios';
+import UserCard from './UserCard';
+const Dashboard = () => {
+return (
+    <div className="dashboard">
+      <h1>AI Dashboard</h1>
+    </div>
+)
+}
+export default Dashboard;
