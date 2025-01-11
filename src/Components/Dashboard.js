fac2805bd17e4fc0c7ee12d3ae6c3a9f10bca5f5
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-11 19:06:30 +0200

    Inializing userStates

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,9 +2,21 @@ import React, { useState, useEffect } from 'react';
 import axios from '../axios';
 import UserCard from './UserCard';
 const Dashboard = () => {
+
+    const [users, setUsers] = useState([]);
+    const [stats, setStats] = useState({
+    detecting: 0,
+    predicting: 0,
+    chatbot: 0,
+    calculating: 0,
+    photoFinish: 0,
+    addingResults: 0,
+    
+  });
 return (
     <div className="dashboard">
       <h1>AI Dashboard</h1>
+      
     </div>
 )
 }
